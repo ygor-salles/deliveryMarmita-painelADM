1c1cada98ee3ea452dbe4c72d76e1b800fa92398
ygor-salles <ygorsallesaniceto@gmail.com>
2021-06-01 23:02:10 -0300

    Criando uma api json de testes com node

--- src/app/models/IadministradorPaginado.model.ts
+++
@@ -1,9 +0,0 @@
-import { Administrador } from './administrador.model';
-export interface IadministradorPaginado {
-  total: number;
-  pagina: number;
-  totalPaginas: number;
-  limite: number;
-  offset: number;
-  instancias: Administrador[];
-}
