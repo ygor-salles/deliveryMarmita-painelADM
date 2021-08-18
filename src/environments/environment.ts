23778d799e6ef1ec1b176fc4ce2eaba21a8acb63
ygor-salles <ygorsallesaniceto@gmail.com>
2021-08-17 22:43:08 -0300

    Alterando as variáveis de ambiente para prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,16 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3000/api',
+//   brasilApiUrl: 'https://brasilapi.com.br/api',
+//   productImagesServerUrl: 'http://localhost:3000/api/',
+//   pedidosTimeout: 30000
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
+  production: true,
+  apiUrl: 'https://back-delivery.herokuapp.com/api',
   brasilApiUrl: 'https://brasilapi.com.br/api',
-  productImagesServerUrl: 'http://localhost:3000/api/',
+  productImagesServerUrl: 'https://back-delivery.herokuapp.com/api/',
   pedidosTimeout: 30000
 };
+
