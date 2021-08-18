8898b455f301f28800070d0a66c38d50dfc27622
ygor-salles <ygorsallesaniceto@gmail.com>
2021-08-17 23:16:29 -0300

    Agora sim

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://back-delivery.herokuapp.com/api',
   brasilApiUrl: 'https://brasilapi.com.br/api',
-  productImagesServerUrl: 'http://localhost:3000/api/',
+  productImagesServerUrl: 'https://back-delivery.herokuapp.com/api/',
   pedidosTimeout: 30000
 };
