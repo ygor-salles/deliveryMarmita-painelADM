8af2d6aab944e6ee8058dbed843bed181dec9102
unknown <ygorsallesaniceto@gmail.com>
2021-10-31 20:01:59 -0300

    mudando variaveis de ambiente da api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://back-delivery.herokuapp.com/api',
+  apiUrl: 'https://delivery-marmita.herokuapp.com',
   brasilApiUrl: 'https://brasilapi.com.br/api',
-  productImagesServerUrl: 'https://back-delivery.herokuapp.com/api/',
+  productImagesServerUrl: 'https://delivery-marmita.herokuapp.com/',
   pedidosTimeout: 30000
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,3 @@ export const environment = {
   productImagesServerUrl: 'http://localhost:3000/api/',
   pedidosTimeout: 30000
 };
-
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://back-delivery.herokuapp.com/api',
-//   brasilApiUrl: 'https://brasilapi.com.br/api',
-//   productImagesServerUrl: 'https://back-delivery.herokuapp.com/api/',
-//   pedidosTimeout: 30000
-// };
-
